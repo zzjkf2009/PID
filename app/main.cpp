b662fdd88dbf6eb19b8f6bc09bf3d441683dbd21
Zejiang Zeng <zzeng@terpmail.umd.edu>
2017-09-26 07:19:38 -0700

    Add source file for PID controller

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,12 @@
-#include <iostream>
+/**@file main.cpp
+ *@brief This is a template main file
+ *@author Zejiang Zeng
+ *@Copyright [2017] <Zejiang Zeng> [MIT]
+ */
 #include <lib.hpp>
+#include <iostream>
 
-int main()
-{
-    dummy();
-    return 0;
+int main() {
+  dummy();
+  return 0;
 }
